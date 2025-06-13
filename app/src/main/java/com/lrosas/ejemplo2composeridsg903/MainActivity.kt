660ba6bee3491dca836adce0e4c11a2fdf1dd18b
IDGS-803-22002362 <81542@alumnos.utleon.edu.mx>
2025-06-12 18:43:52 -0600

    Navegation 12/06/25

---
+++ app/src/main/java/com/lrosas/ejemplo2composeridsg903/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.lrosas.ejemplo2composeridsg903
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.internal.composableLambda
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.composable
+import com.lrosas.ejemplo2composeridsg903.ui.theme.Ejemplo2ComposerIDSG903Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "inicio"){
+                composable("inicio"){PantallaInicio(navController)}
+                composable("detalle/{nombre}"){backStackEntry ->
+                    val nombre = backStackEntry.arguments?.getString("nombre")?:"Invitado"
+                    PantallaDetalle(NavController,nombre)
+                }
+            }
+            }
+        }
+    }
+
+@Composable
+fun PantallaInicio(navController: NavController) {
+    var nombre by remember { mutableStateOf("") }
+    Column (modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center){
+        Text("Pantalla de inicio")
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = nombre,
+            onValueChange = {nombre = it},
+            label = {Text("Introduce tu nombre")}
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            val nombre = nombre.ifBlank { "Betillo" }
+            navController.navigate("detalle/$nombre")
+        }) {
+            Text("Ir a detalle")
+        }
+
+    }
+}
+
+@Composable
+fun PantallaDetalle(navController: NavHostController, nombre: String){
+   Column (modifier = Modifier.fillMaxSize(),
+       horizontalAlignment = Alignment.CenterHorizontally,
+       verticalArrangement = Arrangement.Center){
+        Text("Hola, $nombre")
+        Button(onClick = { navController.navigate("inicio")}) {
+            Text("Volver")
+        }
+   }
+}
+
+
